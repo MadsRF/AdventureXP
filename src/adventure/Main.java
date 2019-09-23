074949c6fe43be83922cf2c203fb7e9472395643
MadsRF <madsrunefrederiksen@gmail.com>
2019-09-23 14:39:59 +0200

    git ignore

---
+++ src/adventure/Main.java
@@ -0,0 +1,9 @@
+package adventure;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("helloo");
+        System.out.println("helloo");
+    }
+}
---
+++ src/adventure/test.java
@@ -0,0 +1,5 @@
+package adventure;
+
+public class test {
+    int i;
+}
