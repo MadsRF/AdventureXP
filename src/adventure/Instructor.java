99884bb6f6ca7bfc250b29396651ba3e8e079631
MadsRF <madsrunefrederiksen@gmail.com>
2019-10-03 10:07:35 +0200

    created Instructor class

---
+++ src/adventure/Instructor.java
@@ -0,0 +1,48 @@
+package adventure;
+
+public class Instructor {
+
+    private String name;
+    private String workDay;
+
+    public Instructor(String name, String workDay) {
+        this.name = name;
+        this.workDay = workDay;
+    }
+
+    public Instructor() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getWorkDay() {
+        return workDay;
+    }
+
+    public void setWorkDay(String workDay) {
+        this.workDay = workDay;
+    }
+
+    public void createInstructor(){
+
+    }
+
+    public void readInstructor(){
+
+    }
+
+    public void updateInstructor(){
+
+    }
+
+    public void deleteInstructor(){
+
+    }
+
+}
