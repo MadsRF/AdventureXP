33f4d53d643f527280236dfd1637d3c277af4912
MadsRF <madsrunefrederiksen@gmail.com>
2019-10-03 11:04:27 +0200

    all around changes

--- src/adventure/Instructor.java
+++ src/adventure/Instructor.java
@@ -1,13 +1,24 @@
 package adventure;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Instructor {
 
     private String name;
-    private String workDay;
+    private String speciality;
+    private Scanner scan = new Scanner(System.in);
+    List<Instructor> instructorList = new ArrayList<>();
 
-    public Instructor(String name, String workDay) {
+
+    public Instructor(String name, String speciality) {
         this.name = name;
-        this.workDay = workDay;
+        this.speciality = speciality;
     }
 
     public Instructor() {
@@ -21,20 +32,63 @@ public class Instructor {
         this.name = name;
     }
 
-    public String getWorkDay() {
-        return workDay;
+    public String getSpeciality() {
+        return speciality;
     }
 
-    public void setWorkDay(String workDay) {
-        this.workDay = workDay;
+    public void setSpeciality(String speciality) {
+        this.speciality = speciality;
     }
 
-    public void createInstructor(){
-
+    public List<Instructor> getInstructorList() {
+        return instructorList;
     }
 
-    public void readInstructor(){
+    public void setInstructorList(List<Instructor> instructorList) {
+        this.instructorList = instructorList;
+    }
 
+    public void createInstructor() {
+        try {
+            System.out.println("Name?");
+            this.name = scan.next();
+            System.out.println("Preferred activity");
+            this.speciality = scan.next();
+            instructorList.add(new Instructor(name, speciality));
+            saveInstructorFile();
+        }catch(Exception e){
+            System.out.println("Something went wrong creating an Instructor");
+        }
+    }
+
+    public void readInstructorFile(){
+        File f = new File("C:\\Users\\madsr\\IdeaProjects\\AdventureXP\\src\\Instructor");
+        try {
+            Scanner s = new Scanner(f);
+            instructorList.clear();
+            while (s.hasNext()) {
+                String name = s.nextLine();
+                String speciality = s.nextLine();
+
+                Instructor i = new Instructor(name, speciality);
+                instructorList.add(i);
+
+                for (Instructor j: instructorList){
+                    System.out.println(j.getName()+" "+j.getSpeciality());
+                }
+            }
+        }
+        catch (Exception e){
+            System.out.println("Something went wrong reading the instructor file");
+        }
+    }
+
+    public void saveInstructorFile() throws FileNotFoundException {
+        PrintStream output = new PrintStream(new FileOutputStream("C:\\Users\\madsr\\IdeaProjects\\AdventureXP\\src\\Instructor", true));
+        for (Instructor i : instructorList) {
+            output.println(i.getName()+" "+i.getSpeciality());
+        }
+        System.out.println();
     }
 
     public void updateInstructor(){
@@ -46,3 +100,5 @@ public class Instructor {
     }
 
 }
+
+
--- src/adventure/Main.java
+++ src/adventure/Main.java
@@ -1,44 +1,16 @@
 package adventure;
 
+import java.io.FileNotFoundException;
+
 public class Main {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
 
         //Aktivitet objekt instansieres
         Activity a = new Activity();
         //Booking objekt instansieres
         Booking b = new Booking();
-
-        //Aktiviteterne oprettes ved opstart
-        /*
-        Activity activity1 = new Activity("Fodbold", 200, 20, "Spille_Fodboold", false);
-        Activity activity2 = new Activity("Basketbold", 200, 20, "Spille_Basket", false);
-        Activity activity3 = new Activity("Håndbold", 200, 20, "Spille_håndbold", false);
-        Activity activity4 = new Activity("Esport", 200, 20, "Spille_pc", false);
-        Activity activity5 = new Activity("Cricket", 200, 20, "spille_cricket", false);
-        */
-        //Bookingerne oprettes ved opstart
-        Booking booking1 = new Booking(1230, 1330, "01-10-2019", 4, "Fodbold");
-        Booking booking2 = new Booking(1330, 1430, "01-10-2019", 32, "Håndbold");
-        Booking booking3 = new Booking(1430, 1530, "01-10-2019", 11, "Esport");
-        Booking booking4 = new Booking(1530, 1630, "01-10-2019", 34, "Cricket");
-        Booking booking5 = new Booking(1630, 1730, "01-10-2019", 27, "Basketbold");
-
-        //Aktiviteterne tilføjes til Arraylisten activityList
-        /*
-        a.getActivityList().add(activity1);
-        a.getActivityList().add(activity2);
-        a.getActivityList().add(activity3);
-        a.getActivityList().add(activity4);
-        a.getActivityList().add(activity5);
-        */
-        //Bookingerne tilføjes til Arraylisten bookingList
-        b.getbookingList().add(booking1);
-        b.getbookingList().add(booking2);
-        b.getbookingList().add(booking3);
-        b.getbookingList().add(booking4);
-        b.getbookingList().add(booking5);
 
         //Hovedmenuen instatieres og køres
         Menu menu = new Menu();
